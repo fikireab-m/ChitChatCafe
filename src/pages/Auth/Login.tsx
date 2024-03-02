857b12ae26806f3f7a884e6d864a751ad9585635
fikireab-m <fikireab.meku@gmail.com>
2024-03-02 19:29:30 +0300

    styling inputs

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -3,14 +3,14 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import { Visibility, VisibilityOff, LockOutlined, AlternateEmailOutlined } from '@mui/icons-material';
 
 function Copyright() {
   return (
@@ -26,6 +26,14 @@ function Copyright() {
 }
 
 const Login = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  }
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -60,44 +68,70 @@ const Login = () => {
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-            alignSelf:'center',
+            alignSelf: 'center',
             maxWidth: 420,
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
+          <Avatar sx={{ m: 1, bgcolor: 'primary.main', height: 64, width: 64 }}>
+            <LockOutlined sx={{ fontSize: "3rem" }} />
           </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign In
+          <Typography component="h1" variant="h5" sx={{ color: "var(--lightText)", fontWeight: "800" }}>
+            Welcome back
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1}}>
+          <Typography component="h1" variant="body2" sx={{ color: "var(--lightGreyText)", mt: 0.5 }}>
+            Sign in to your account
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ p: 2 }}>
             <TextField
               margin="normal"
               required
               fullWidth
               size="small"
+              color='primary'
               id="username"
-              label="Username"
-              name="username"
-              placeholder="Username / email"
-              autoComplete="username"
+              label="Email address"
+              name="email"
+              type="email"
+              placeholder="Email address"
+              autoComplete="email"
               autoFocus
-              sx={{fontSize:'1rem', borderRadius:'1rem'}}
+              InputProps={{
+                startAdornment:
+                  <InputAdornment position="start" >
+                    < AlternateEmailOutlined sx={{ color: 'primary.main' }} />
+                  </InputAdornment>
+              }}
             />
             <TextField
               margin="dense"
               required
               fullWidth
               size="small"
+              color='primary'
               name="password"
               label="Password"
-              type="password"
               id="password"
+              type={showPassword ? 'text' : 'password'}
+              InputProps={{
+                endAdornment:
+                  <InputAdornment position="end" >
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                      sx={{ color: 'primary.main' }}
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>,
+                startAdornment:
+                  <InputAdornment position="start" >
+                    <LockOutlined sx={{ color: 'primary.main' }} />
+                  </InputAdornment>
+              }}
+
               autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
             />
             <Button
               type="submit"
@@ -123,7 +157,7 @@ const Login = () => {
           </Box>
         </Box>
       </Grid>
-    </Grid>
+    </Grid >
   );
 }
 
