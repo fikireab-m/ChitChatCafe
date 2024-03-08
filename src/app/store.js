5fee6b85cf33424705f3c891777f94548a3b9db1
fikireab-m <fikireab.meku@gmail.com>
2024-03-08 15:38:41 +0300

    api call fix

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleWare from "redux-saga";
 import AuthReducer from "../features/auth/authSlice";
-import rootSaga from "../features/rootSaga";
+import Saga from "../features/rootSaga";
 
 const saga = createSagaMiddleWare()
 const store = configureStore({
@@ -13,5 +13,5 @@ const store = configureStore({
     }).concat(saga),
     devTools:true,
 });
-saga.run(rootSaga)
+saga.run(Saga)
 export default store;
